81819de261ab0c64b4e045230dbb2e49319d75c6
Mario Car <mario.car@ericsson.com>
2021-07-17 11:56:22 +0200

    Explicitly set the mark

--- radian.el
+++ radian.el
@@ -5130,6 +5130,23 @@ bound dynamically before being used.")
 ;; by changing packages upstream.
 (setq ad-redefinition-action 'accept)
 
+;; Explicitly set the mark
+(defun push-mark-no-activate ()
+  "Pushes 'point' to 'mark-ring' and does not activate the
+region. Equivalent to \\[set-mark-command] when
+\\[transient-mark-mode] is disabled."
+  (interactive)
+  (push-mark (point) t nil)
+  (message "Pushed mark to ring"))
+(defun jump-to-mark ()
+  "Jumps to the local mark, respecting the 'mark-ring' order.
+This is the same as using \\[set-mark-command] with the prefix
+argument."
+  (interactive)
+  (set-mark-command 1))
+(global-set-key (kbd "C-`") 'push-mark-no-activate)
+(global-set-key (kbd "M-`") 'jump-to-mark)
+
 ;;; Appearance
 
 ;; Make the initial frame maximized, unless using the Mac port in
