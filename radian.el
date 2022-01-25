3b6d99bb6f806be662a44ca8becbc5e128e79b82
Mario Car <mario.car@ericsson.com>
2022-01-25 12:49:39 +0100

    exchange point and mark but don't activate region
    This idea is borrowed from "MasteringEmacs"

--- radian.el
+++ radian.el
@@ -5256,14 +5256,20 @@ region. Equivalent to \\[set-mark-command] when
   (interactive)
   (push-mark (point) t nil)
   (message "Pushed mark to ring"))
+(global-set-key (kbd "C-`") 'push-mark-no-activate)
 (defun jump-to-mark ()
   "Jumps to the local mark, respecting the 'mark-ring' order.
 This is the same as using \\[set-mark-command] with the prefix
 argument."
   (interactive)
   (set-mark-command 1))
-(global-set-key (kbd "C-`") 'push-mark-no-activate)
 (global-set-key (kbd "M-`") 'jump-to-mark)
+(defun exchange-point-and-mark-no-activate ()
+  "Identical to \\[exchange-point-and-mark] but will not activate the region."
+  (interactive)
+  (exchange-point-and-mark)
+  (deactivate-mark nil))
+(define-key global-map [remap exchange-point-and-mark] 'exchange-point-and-mark-no-activate)
 
 ;; For commands that are useful to use in continuous stream,
 ;; defrepeater helps in a way that you don't need to press complete
