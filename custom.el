e2101aa72fe2f01d8111d3f93b402df41e53dc32
Mario Car <mario.car@ericsson.com>
2023-07-12 12:23:11 +0200

    Track custom.el
    Configuration is not "portable" if custom.el is missing.

---
+++ custom.el
@@ -0,0 +1,13 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(kind-icon cape yasnippet-snippets avy-zap buffer-move consult-projectile corfu dumb-jump eglot-java embark-consult expand-region git-gutter google-translate helpful macrostep magit marginalia orderless rg transpose-frame vertico visual-regexp-steroids yasnippet)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
