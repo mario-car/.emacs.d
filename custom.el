75dea7ca9bb1b01e52b912e4a475d3afcc2409f2
Mario Car <mario.car@ericsson.com>
2023-07-16 22:22:19 +0200

    Update `package-selected-packages' list

--- custom.el
+++ custom.el
@@ -4,7 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(kind-icon cape yasnippet-snippets avy-zap buffer-move consult-projectile corfu dumb-jump eglot-java embark-consult expand-region git-gutter google-translate helpful macrostep magit marginalia orderless rg transpose-frame vertico visual-regexp-steroids yasnippet)))
+   '(avy-zap buffer-move cape consult-projectile corfu dumb-jump eglot-java embark-consult expand-region git-gutter google-translate helpful kind-icon macrostep magit marginalia orderless rg transpose-frame vertico visual-regexp-steroids yasnippet-snippets)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
