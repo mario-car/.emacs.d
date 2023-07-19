c221c05fe93244e94a4924453c2bffa18c388e19
Mario Car <mario.car@ericsson.com>
2023-07-19 07:28:13 +0200

    Increased 'max-lisp-eval-depth'
    Emacs was designed to run on machines that we now
    consider limited and its default settings are conservative.  You may
    want to increase the value of ‘max-lisp-eval-depth’.  In my ‘.emacs’
    file, I set it to 30 times its default value.
    
    Suggestion taken from "An Introduction to Programming in Emacs Lisp"

--- custom.el
+++ custom.el
@@ -3,6 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(max-lisp-eval-depth 48000)
  '(package-selected-packages
    '(avy-zap buffer-move cape consult-projectile corfu dumb-jump eglot-java embark-consult expand-region git-gutter google-translate helpful kind-icon macrostep magit marginalia orderless rg transpose-frame vertico visual-regexp-steroids yasnippet-snippets)))
 (custom-set-faces
