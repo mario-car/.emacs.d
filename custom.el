4eaef2984f9659ed0037fbe177ff5d63fdd8f4f2
Mario Car <mario.car.ext@ericsson.com>
2024-11-30 13:48:20 +0100

    update custom settings

--- custom.el
+++ custom.el
@@ -82,9 +82,12 @@
  '(max-lisp-eval-depth 48000)
  '(package-selected-packages
    '(use-package-chords elfeed kind-icon consult-eglot avy-zap buffer-move cape consult-projectile corfu dumb-jump eglot-java embark-consult expand-region git-gutter google-translate helpful macrostep magit marginalia orderless rg transpose-frame vertico visual-regexp-steroids yasnippet-snippets)))
+ '(project-ignore-buffer-conditions '("/target/"))
+ '(project-vc-ignores '("target/"))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+ '(cursor ((t (:background "magenta")))))
