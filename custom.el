764aae5bb11ebc15ee147355fb5fec2850a73104
Mario Car <mario.car.ext@ericsson.com>
2025-02-02 10:34:15 +0100

    install vterm package
    just as a test, to replace rare tmux needs with it

--- custom.el
+++ custom.el
@@ -7,8 +7,9 @@
    '(avy-zap bash-completion bind-chord buffer-move cape corfu dumb-jump
 	     eglot-java elfeed embark-consult expand-region git-gutter
 	     helpful key-chord kind-icon macrostep magit marginalia
-	     orderless rg transpose-frame use-package-chords vertico
-	     visual-regexp-steroids yasnippet yasnippet-snippets)))
+	     multi-vterm orderless rg transpose-frame
+	     use-package-chords vertico visual-regexp-steroids vterm
+	     yasnippet yasnippet-snippets)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
