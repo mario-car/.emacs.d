6e234bcf3e27b0264c80089943dca209f830569f
Mario Car <mario.car.ext@ericsson.com>
2025-02-02 10:35:24 +0100

    add languages to `org-babel-load-languages'

--- custom.el
+++ custom.el
@@ -3,6 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(org-babel-load-languages '((java . t) (shell . t) (emacs-lisp . t)))
  '(package-selected-packages
    '(avy-zap bash-completion bind-chord buffer-move cape corfu dumb-jump
 	     eglot-java elfeed embark-consult expand-region git-gutter
