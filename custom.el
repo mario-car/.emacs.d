11bd0f0dd4fd06b071639c706709e2f62dab3c31
Mario Car <mario.car.ext@ericsson.com>
2025-03-18 18:24:56 +0100

    custom update

--- custom.el
+++ custom.el
@@ -4,15 +4,20 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(org-babel-load-languages '((java . t) (shell . t) (emacs-lisp . t)))
+ '(calendar-week-start-day 1)
  '(package-selected-packages
-   '(avy-zap bash-completion bind-chord buffer-move cape cider codium
-	     corfu dumb-jump eglot-java elfeed embark-consult
-	     expand-region git-gutter helpful key-chord kind-icon
-	     lsp-java lsp-mode macrostep magit marginalia 
-	     orderless rg transpose-frame use-package-chords vertico
-	     visual-regexp-steroids yasnippet yasnippet-snippets))
-  '(package-vc-selected-packages
-   '((eglot-booster :vc-backend Git :url "https://github.com/jdtsmith/eglot-booster.git")))
+   '(all-the-icons all-the-icons-completion all-the-icons-nerd-fonts
+		   avy-zap bash-completion bind-chord buffer-move cape
+		   cider codium consult-eglot consult-eglot-embark
+		   corfu dumb-jump eglot eglot-java elfeed
+		   embark-consult expand-region git-gutter helpful
+		   key-chord kind-icon lsp-java lsp-mode macrostep
+		   magit marginalia orderless rg transpose-frame
+		   use-package-chords vertico visual-regexp-steroids
+		   yasnippet yasnippet-snippets))
+ '(package-vc-selected-packages
+   '((eglot-booster :vc-backend Git :url
+		    "https://github.com/jdtsmith/eglot-booster.git")))
  '(project-ignore-buffer-conditions '("/target/"))
  '(project-vc-ignores '("target/")))
 (custom-set-faces
