87f922aeb60c97c568da0b7039d3499edda25275
Mario Car <mcar0503@gmail.com>
2025-04-08 13:35:20 -0700

    update custom.el

--- custom.el
+++ custom.el
@@ -3,20 +3,21 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(org-babel-load-languages '((java . t) (shell . t) (emacs-lisp . t)))
  '(calendar-week-start-day 1)
+ '(org-babel-load-languages '((java . t) (shell . t) (emacs-lisp . t)))
  '(package-selected-packages
    '(all-the-icons all-the-icons-completion all-the-icons-nerd-fonts
 		   avy-zap bash-completion bind-chord buffer-move cape
 		   cider codium consult-eglot consult-eglot-embark
 		   corfu dumb-jump eglot eglot-java elfeed
 		   embark-consult expand-region git-gutter helpful
-		   key-chord kind-icon lsp-java lsp-mode macrostep
-		   magit marginalia orderless rg transpose-frame
-		   use-package-chords vertico visual-regexp-steroids
-		   yasnippet yasnippet-snippets))
+		   key-chord kind-icon macrostep magit marginalia
+		   orderless rg transpose-frame use-package-chords
+		   vertico visual-regexp-steroids vterm yasnippet
+		   yasnippet-snippets))
  '(package-vc-selected-packages
-   '((eglot-booster :vc-backend Git :url
+   '((codium :url "https://github.com/Exafunction/codeium.el")
+     (eglot-booster :vc-backend Git :url
 		    "https://github.com/jdtsmith/eglot-booster.git")))
  '(project-ignore-buffer-conditions '("/target/"))
  '(project-vc-ignores '("target/")))
